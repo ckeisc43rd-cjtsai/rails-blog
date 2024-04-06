a59dfd512a023a4bc9a991f6e75b55e92292a055
cjtsai <jimtsai9596@gmail.com>
2024-04-06 16:43:13 +0800

    modify for deploying on digital ocean

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,13 +6,12 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.force_ssl = true
+#  config.force_ssl = true
   config.serve_static_files = true
   config.enable_reloading = true
-#  config.hosts << "rb"
 config.hosts << "rb"
 config.action_controller.default_url_options= {host: 'ruby.cjtsai.com'}
-    config.hosts << "ruby.cjtsai.com"
+config.hosts << "ruby.cjtsai.com"
 config.action_controller.forgery_protection_origin_check = false
   # Do not eager load code on boot.
   config.eager_load = false
